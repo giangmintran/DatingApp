b385e51fbd8d3c883423b650e5c9f7d8e2fb23c7
giangmintran <86286037+giangmintran@users.noreply.github.com>
2021-06-30 16:43:57 +0700

    End Of Section 4

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class UserDto
+    {
+        public String Username { get; set; }
+
+        public String Token { get; set; }
+    }
+}
